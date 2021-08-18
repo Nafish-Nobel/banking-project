398a9ec58a55ff9d896affa0fe697954e4833716
Nafish-Nobel <nobel15-11651@diu.edu.bd>
2021-08-18 11:55:54 +0600

    add project

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.getElementById('login-submit').addEventListener('click', function () {
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
     // check email and password
-    if (userEmail == '' && userPassword == '') {
+    if (userEmail == 'sija@gmail.com' && userPassword == 'sija') {
         window.location.href = 'banking.html';
     }
 });
